d2c5ce88493338fa41e59013a12447559cf5eec7
Sean Burke <sburke@8thwonder.io>
2017-08-28 01:17:05 -0400

    Started a trait for standardizing responses from the api

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,23 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Model\Project;
+use App\Traits\APIResponse;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
+    use APIResponse;
     /**
      * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        return response()->json(Project::all());
+        return $this->apiResponse(Project::all());
     }
 
+    /**
+     * @param Project $project
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function get(Project $project)
     {
         return response()->json($project);
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function create(Request $request)
     {
         $validator = \Validator::make($request->all(), [
@@ -30,12 +40,27 @@ class ProjectController extends Controller
             return response()->json(['errors' => $validator->getMessageBag()->all()], 400);
         }
         $project = Project::create($request->all());
-        return response()->json($project, 201);
+        return response()->json($project, APIResponse::$CREATED);
     }
 
+    /**
+     * @param Request $request
+     * @param Project $project
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(Request $request, Project $project)
     {
         $project->update($request);
         return response()->json($project, 200);
+    }
+
+    /**
+     * @param Project $project
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function delete(Project $project)
+    {
+        $project->delete();
+        return response()->json('deleted', 200);
     }
 }
---
+++ app/Traits/APIResponse.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sean
+ * Date: 8/27/17
+ * Time: 10:44 PM
+ */
+
+namespace App\Traits;
+
+
+trait APIResponse
+{
+    //Response codes, work around for not having const in traits
+    public static $OK = 200;
+    public static $CREATED = 201;
+    public static $ACCEPTED = 202;
+    public static $NOCONTENT = 204;
+    public static $UNAUTHORIZED = 401;
+    public static $FORBIDDEN = 403;
+    public static $NOTFOUND = 404;
+
+    /**
+     * @param array $data
+     * @param int $code
+     * @param string $status
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function apiResponse(array $data, int $code = 200, string $status = 'success')
+    {
+        $response = [
+            'datetime' => new \DateTime('U'),
+            'status' => $status,
+            'code' => $code
+        ];
+
+        if ($data !== null) {
+            $response['data'] = $data;
+        }
+        return response()->json($response, $code);
+    }
+
+    /**
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function apiError(string $message)
+    {
+        return $this->apiResponse(['message' => $message], self::$NOTFOUND, 'error');
+    }
+
+    public function apiCreated(array $data)
+    {
+        
+    }
+}
