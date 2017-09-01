fdfa0e65ac7419165071f5a67709e8b8eec03d20
Sean Burke <sburke@8thwonder.io>
2017-09-01 00:17:40 -0400

    Tweaks to the team controller to add it owner checking. Need to eventually add in a full permissions system.

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Model\Team;
 use App\Traits\APIResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class TeamController extends Controller
+class TeamController extends BaseController
 {
     use APIResponse;
 
@@ -31,7 +32,17 @@ class TeamController extends Controller
     public function create(Request $request)
     {
         try {
-            $team = Team::create($request->all());
+            $validator = \Validator::make($request->all(), [
+               'name' => 'required'
+            ]);
+
+            if ($validator->fails()) {
+                return $this->apiError($validator->getMessageBag()->all());
+            }
+            $ownerId = $this->getUser()->id;
+            $data = array_merge($request->all(), ['owner_id' => $ownerId]);
+            $team = Team::create($data);
+            $team->users()->attach($ownerId);
             return $this->apiResponse($team);
         } catch (\Throwable $e) {
             return $this->apiError($e->getMessage());
@@ -45,9 +56,11 @@ class TeamController extends Controller
      */
     public function update(int $team, Request $request)
     {
-        //Todo: Make sure that the user is the owner
         try {
             $team = Team::findOrFail($team);
+            if ($team->owner_id !== $this->getUser()->id) {
+                return $this->apiError('User is not the owner of the team');
+            }
             $team->update($request->all());
             return $this->apiResponse($team);
         } catch (\Throwable $e) {
@@ -65,6 +78,25 @@ class TeamController extends Controller
             $team = Team::findOrFail($team);
             $team->delete();
             return $this->apiDelete('Team deleted');
+        } catch (\Throwable $e) {
+            return $this->apiError($e->getMessage());
+        }
+    }
+
+    /**
+     * @param int $team
+     * @param int $user
+     * @return JsonResponse
+     */
+    public function addUser(int $team, int $user): JsonResponse
+    {
+        try {
+            $team = Team::findOrFail($team);
+            if ($team->owner()->get('id') !== $this->getUser()->id) {
+                return $this->apiError('User is not the owner of the team');
+            }
+            $team->users()->attach($user);
+            return $this->apiResponse(['message' => 'User added to the team']);
         } catch (\Throwable $e) {
             return $this->apiError($e->getMessage());
         }
